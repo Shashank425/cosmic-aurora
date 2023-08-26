3061b6b39ccaa02f12b6b5a2d384337719b8e6cb
Shashank425 <shashank.kathavate@gmail.com>
2023-08-26 06:25:59 -0500

    nav bar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavBarTopComponent } from './nav-bar-top/nav-bar-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavBarTopComponent
   ],
   imports: [
     BrowserModule,
